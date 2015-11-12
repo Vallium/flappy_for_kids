ff3733e2a3b7c7fa5b18379a1f5120cfd190d3f7
Vallium <anton.alliot@gmail.com>
2015-11-12 20:45:37 +0100

    comments

--- main.lua
+++ main.lua
@@ -1,17 +1,37 @@
-require "utils"
+-- #############################################################################
+-- ############################## FLAPPY FOR KIDS ##############################
+-- #############################################################################
+
+require "utils" -- PAS TOUCHE A CA
 
 function		love.load()
+-- Cette fonction est appelee une fois au demarage, elle permet de mettre
+-- en place tout ce qui sera necessaire au bon fonctionnement du programme
+
+-- Ecrivez votre code ici!!
 
 end
 
 function		love.update(dt)
+-- Cette fontion est appelee a chaque frame, c'est ici que le programme
+-- est actualise. dt est la variable qui contient le temps ecoule
+
+-- Ecrivez votre code ici!!
 
 end
 
 function		love.draw()
+-- Cette fonction est aussi appelee a chaque frame, mais a la difference
+-- d'update(), ici on s'occupe seulement de l'affichage
+
+-- Ecrivez votre code ici!!
 
 end
 
 function		love.keypressed(key)
+-- Cette fonction est appelee a chaque fois que l'utlisateur appuie sur une
+-- touche du clavier. La variable key contient le nom de cette touche
+
+-- Ecrivez votre code ici!!
 
 end
